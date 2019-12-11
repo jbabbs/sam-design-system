293699e4a215ed4d644f3ec278ac8917704e904e
Yerram_Shilpa <yerram_shilpa@Shilpas-MacBook-Pro.local>
2019-12-11 13:12:44 -0500

    updated formly

---
+++ src/formly/formly-filters/formly-filter.md
@@ -0,0 +1,1 @@
+Formly- filter overview document
---
+++ src/formly/formly-filters/public-api.ts
@@ -0,0 +1,11 @@
+/**
+ * @license
+ * Copyright [COPYRIGHT HOLDER] All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at [LINK TO LICENSE]
+ */
+
+export * from './sds-filters.component';
+export * from './sds-filters.component.spec';
+export * from './sds-filters.module';
