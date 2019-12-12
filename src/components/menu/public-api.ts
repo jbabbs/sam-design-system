b5bbc467ca154ec0de7e1b27303d8f7d5c55bd29
Carlos Valle <carlos.m.valle@gmail.com>
2019-12-12 09:40:50 -0500

    Add needed files to the menu component

---
+++ src/components/menu/public-api.ts
@@ -0,0 +1,14 @@
+/**
+ * @license
+ * Copyright [COPYRIGHT HOLDER] All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at [LINK TO LICENSE]
+ */
+
+export * from './menu-animations';
+export * from './menu-header.component';
+export * from './menu-item.component';
+export * from './menu-trigger.directive';
+export * from './menu.component';
+export * from './menu.module';
