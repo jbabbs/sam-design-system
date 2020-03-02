c81e2066cd5e481076fa7c65466c7ffe0be9e0c7
Hemal1975 <hemal6512@gmail.com>
2020-03-02 10:05:39 -0500

    Update strings.js

--- libs/packages/components/src/lib/video-player/js/strings.js
+++ libs/packages/components/src/lib/video-player/js/strings.js
@@ -1,19 +1,10 @@
 export var GLOBAL_STRINGS = {
-
   PLAY: "Play",
-
   PAUSE: "Pause",
-
   TOGGLE_FULL_SCREEN: "Toggle full screen",
-
   MUTE: "Mute",
-
   RESTART: "Restart",
-
   CAPTIONS: "Closed captions",
-
   REWIND: "Rewind",
-
   FORWARD: "Forward"
-
 };
